8a081f37574785e9468b0041587d1fd85d035cca
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-09-28 06:14:35 -0400

    build updates

---
+++ build/precache-manifest.bb02a236f1f12e4d22e1c6bdc52b7120.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db4e6880ce3cbe3cb7fda8cf6c38cd43",
+    "url": "/cn-portfolio/index.html"
+  },
+  {
+    "revision": "f6f9fa346c186a66ccd1",
+    "url": "/cn-portfolio/static/css/main.9c7be960.chunk.css"
+  },
+  {
+    "revision": "c879d648e9900ef95028",
+    "url": "/cn-portfolio/static/js/2.c43e596b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cn-portfolio/static/js/2.c43e596b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6f9fa346c186a66ccd1",
+    "url": "/cn-portfolio/static/js/main.b54a95c3.chunk.js"
+  },
+  {
+    "revision": "174a9ff095ad23d30ad8",
+    "url": "/cn-portfolio/static/js/runtime-main.ebfe42d7.js"
+  }
+]);
